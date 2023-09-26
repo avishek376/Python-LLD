5316b61e630b441cb677b8be50b88ec9047b8a97
avishek376 <avishekdeb376@gmail.com>
2023-09-26 23:15:47 +0530

    Abstract Factory push

--- Abstract Factory Design Pattern/impl_table_a.py
+++ Abstract Factory Design Pattern/impl_table_a.py
@@ -1,4 +1,20 @@
-class OvalStoneTable:
+from interface_table import ITable
+
+
+class OvalStoneTable(ITable):
+    """
+    Concrete implementation of OvalStoneTable
     """
 
-    """
+    def __init__(self):
+        self.width = 36
+        self.length = 36
+
+    def build_table(self):
+        """
+
+        :return: instance of SquareWoodenTable
+        """
+        return {
+            "Oval_Stone_Table_top_only": f"width {self.width} inches and length{self.length} inches"
+        }
