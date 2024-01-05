6f965c31a1f5da34db136f3ac1fae4899f4dd0dc
avishek376 <avishekdeb376@gmail.com>
2024-01-06 01:21:00 +0530

    Singleton design pattern by Decorators in python

--- Singleton Design Pattern/Singleton-Decorator/Singleton.py
+++ Singleton Design Pattern/Singleton-Decorator/Singleton.py
@@ -14,9 +14,14 @@ class MongoDB:
     def __init__(self, instance_number) -> None:
         print(f"Mongo DB instance number {instance_number} created")
 
+    @classmethod
+    def mongo_db_class_method(cls, data):
+        print(f"class method calls {data}")
+
 
 if __name__ == "__main__":
     mongo_db_conn1 = MongoDB(1)
     mongo_db_conn2 = MongoDB(2)
+    # MongoDB.mongo_db_class_method(5)
 
     print(mongo_db_conn1 == mongo_db_conn2)
