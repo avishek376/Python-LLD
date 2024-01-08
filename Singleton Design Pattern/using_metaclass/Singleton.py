549299a3d512d10831042240a15f7bce0895afaf
avishek376 <avishekdeb376@gmail.com>
2024-01-08 12:49:19 +0530

    Singleton design pattern by metaclass in python

--- Singleton Design Pattern/using_metaclass/Singleton.py
+++ Singleton Design Pattern/using_metaclass/Singleton.py
@@ -1,26 +1,32 @@
-class SingletonMeta(type):
+class StudentSingletonMeta(type):
+    """This is a metaclass that will be used to create singleton classes"""
     _instances = {}
 
     def __call__(cls, *args, **kwargs):
+        """This method will be called when we try to create an instance of the class"""
         if cls not in cls._instances:
-            instance = super().__call__(*args, **kwargs)
-            cls._instances[cls] = instance
-
+            cls._instances[cls] = super(StudentSingletonMeta, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
 
 
-class Singleton(metaclass=SingletonMeta):
-    def some_business_logic(self):
-        pass
+class StudentSingleton(metaclass=StudentSingletonMeta):
+    """This is a singleton class"""
+
+    def __init__(self, *args, **kwargs):
+        self.first_name = args[0]
+        self.last_name = args[1]
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 
-if __name__ == "__main__":
-    # The client code.
+student1 = StudentSingleton("Ajay", "Biswas")
+student2 = StudentSingleton("Amit", "Roy")
 
-    s1 = Singleton()
-    s2 = Singleton()
+print((student1))
+print((student2))
 
-    if id(s1) == id(s2):
-        print("Singleton works, both variables contain the same instance.")
-    else:
-        print("Singleton failed, variables contain different instances.")
+if id(student1) == id(student2):
+    print("Singleton worked,Both the instances are same")
+else:
+    print("Singleton failed,Both the instances are different")
