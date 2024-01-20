6f7059623007e5ed01df5014d04429a9671366bb
avishek376 <avishekdeb376@gmail.com>
2024-01-20 16:17:55 +0530

    Practical Abstract Factory Design Pattern

--- Abstract Factory Design Pattern/furniture-factory-usecase/ITable.py
+++ Abstract Factory Design Pattern/furniture-factory-usecase/ITable.py
@@ -4,6 +4,7 @@ from abc import ABC, abstractmethod
 class ITable(ABC):
     """This is the common Abstract Class for Tables"""
 
+    @staticmethod
     @abstractmethod
     def create(self):
         """This is the table method"""
